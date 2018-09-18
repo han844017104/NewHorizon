1627803b3ec1ddd6dcd5a04cd8dfb3edec89532f
Mr.Han <844017104@qq.com>
2018-09-18 17:30:40 +0800

    New实体类调整 By:Mr.Han

--- NewHorizon_Domain/src/main/java/com/qfedu/newhorizon/domain/news/New.java
+++ NewHorizon_Domain/src/main/java/com/qfedu/newhorizon/domain/news/New.java
@@ -5,16 +5,6 @@ import java.util.Date;
 public class New {
     private Integer nid;
 
-    private String title;
-
-    private Date creattime;
-
-    private Integer type;
-
-    private Integer flag;
-
-    private String text;
-
     public Integer getNid() {
         return nid;
     }
@@ -23,43 +13,4 @@ public class New {
         this.nid = nid;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
-
-    public Date getCreattime() {
-        return creattime;
-    }
-
-    public void setCreattime(Date creattime) {
-        this.creattime = creattime;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public Integer getFlag() {
-        return flag;
-    }
-
-    public void setFlag(Integer flag) {
-        this.flag = flag;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text == null ? null : text.trim();
-    }
 }
